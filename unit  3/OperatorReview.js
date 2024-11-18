6defcf20e4ec65bcc8522ac4fc4f2ec2c4d42662
NashaanO <Noglesby527@boyslatin.net>
2024-11-18 18:39:19 +0000

    Async activity Nov8

---
+++ unit  3/OperatorReview.js
@@ -0,0 +1,25 @@
+//Arithmetic operators-Writing code expressions
+//  that perform math operations
+//Example of apps that use arithmatic operators
+
+//Depositing money into a digital account
+var depositeamount=2000
+
+var  checkingsAccount=2000
+
+var savingsAccount=2000
+
+console.log(depositeAmount + CheckingAccount)
+//Function- set of instructions that perform a job 
+function bankapp(){
+    if(Userselection)
+}
+
+
+// peer to peer money transfer
+var amountBeingSent= 2000
+var UserAccountTotal=2000
+
+//Asssignment- giving variables values.
+// we use the single signal
+
