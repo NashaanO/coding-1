cbe60b430cd770ca6ccf4ca906fb35bab6ebc3e4
NashaanO <Noglesby527@boyslatin.net>
2024-11-19 20:13:26 +0000

    javascript Practice compeleted

---
+++ unit  3 js/AsyncActivityNov15.js
@@ -0,0 +1,19 @@
+var username="Dxnero"
+var username="ball"
+
+var SecuritySystemUsername='Dxnero'
+var SecuritySystemUsername='ball'
+
+console.log(username)
+
+
+
+//2-Compairison
+var echOrderRequest='3'
+var ecgShipmentRecived='20'
+
+
+//3-'assignment opertor'
+
+var messageToSend= 'help'
+var MessageToReceive= messageToSend
---
+++ unit  3 js/functionPractice/function.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>-
+<html>
+<head>
+<title></title>
+</head>
+<body>
+
+<h1>JavaScript Practice</h1>
+<script src="function.js"></script>
+
+</body>
+</html>
