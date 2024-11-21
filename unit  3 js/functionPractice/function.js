653213948529a4f79e2a831ba9e1ebbc224a08d4
NashaanO <Noglesby527@boyslatin.net>
2024-11-21 20:19:06 +0000

    Edited code bank assignment

--- unit  3 js/functionPractice/function.js
+++ unit  3 js/functionPractice/function.js
@@ -1,12 +1,31 @@
-<!DOCTYPE html>-
-<html>
-<head>
-<title></title>
-</head>
-<body>
+// What is function?
+'A function is a set instructions that performs a specific task'
+'or job.'
 
-<h1>JavaScript Practice</h1>
-<script src="function.js"></script>
+// Functions come in 2 variations:
+// built-in functions- These are instructions that are pre-written
+// into the coding language, All we need to do is pass in data. 
 
-</body>
-</html>
+// examples
+var className= 'Coding class'
+console.log(className)
+ 
+// user-defined functions- These are custom instructions written by
+// engineers to make code that performs specific tasks. 
+
+// How do we write a user-defined function?
+function exampleFunction(){
+    console.log('The function script is connected.')
+}
+
+// in order to have a function work, we need to call it. 
+// the above code is the function definition- MEANING- it's only
+// telling the computer the instructions- NOT to actually run it. 
+
+// we access or call a function by writing its name.
+//exampleFunction()
+let accountBalance = 1000;
+
+function depositMoney() {
+  accountBalance += 200;
+  document.getElementById('data').innerHTML = `Current balance: $${accountBalance}`;
