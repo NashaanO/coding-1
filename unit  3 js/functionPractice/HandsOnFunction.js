aa8bb9537294d228203f37f7ef3d586077843f6a
NashaanO <Noglesby527@boyslatin.net>
2024-12-03 20:04:48 +0000

    HandsOnFunction

---
+++ unit  3 js/functionPractice/HandsOnFunction.js
@@ -0,0 +1,16 @@
+// Anaromy of a function
+
+// curly brackets=this is
+// called a parameter. we pass data
+// inside of the function parameters
+
+// parameter=place holder for data
+
+// data= just means data types
+
+function pizzaparty(name, topping){
+    console.log('congrats '+ name + 'you have been invited to the pizza party')
+    console.log('your pizza will have '+ topping + "on it");
+}
+
+pizzaparty('Veggies')
