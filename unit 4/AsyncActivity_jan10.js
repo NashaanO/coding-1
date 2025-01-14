cb2a985dda9c6d90a7eb01d0671760ea7631883f
NashaanO <Noglesby527@boyslatin.net>
2025-01-14 20:10:10 +0000

    AsyncActivty_jan10 Submited

---
+++ unit 4/AsyncActivity_jan10.js
@@ -0,0 +1,19 @@
+//1.
+// Alist is adata type that can be used to group and collect other 
+// data types together in one variable.
+// list can contain all data types, including variables and other lists
+
+var internationalFood = [' Ramen' ,'chili sauce', 'soy sauce', 'old bay']
+var produce =['apples', 'bell peppers', 'mushgroom']
+groceryShoppinglist =[internationalFood, produceFood];
+console.log(groceryShoppinglist)
+
+//2.
+// index 0= 1 
+//index 3= 50
+// index 4 = 293
+
+//.3
+
+function multiplier (x){
+    console.log (x * 293);
---
+++ unit 4/javacriptLoop.js
@@ -0,0 +1,41 @@
+function multiplier (x){
+    console.log (x * 293);
+}      function sophmore(year){ 
+  if(year==1){
+    console.log(" your a freshman");
+  } else if ("year==2."){
+    console.log("sophmore.");
+  }else if (year==3){
+    console.log('your a junior');
+  }else if(year == 4){
+    console.log('your a senior');
+  }else if(year=5){
+    console.log ("your in a masters program")
+  }
+
+    //console.log ('testing function to see if it works:' )
+}
+
+
+collegeTitle(7)
+
+// a JS list is a data type that is used to group and
+// organize other data types into one variable
+
+// we use the square brackets to designate something as 
+// a list.
+var grocerylist= [ 'water', 'apple', "ground beef"]
+var RandomData =[Jamal,'coding class',
+true,
+false,
+ ]
+ console.log(RandomData)
+ coding1s=['black cw,'green cw','white cw']
+ coding2s=['red cw ','bluecw']
+ codings3s=[]
+
+ console.log('theses are the coding sneakers we have in stock')
+ console.log(codings1 , codings2 , codings3,)
+
+ cleaning supplies= ['dish soap', 'paper towels',]
+ deli meats = ['turkey', 'ham',]
