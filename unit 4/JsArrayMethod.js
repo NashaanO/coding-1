47cb46d7e1740e7690abc0362d9b1fff463fb9a5
NashaanO <Noglesby527@boyslatin.net>
2025-01-16 20:06:24 +0000

    JsArray notes

---
+++ unit 4/JsArrayMethod.js
@@ -0,0 +1,52 @@
+// Arrays - A special data type for 
+// collecting and strong data inside
+// of a variable
+
+//we declare am array by giving it a 
+// variable name, and then assigning it the
+// square brackets
+var numbers = [1,222,2,22222,222222]
+var randomData =[true, 'name', numbers]
+
+//What is a method?
+
+
+// push
+
+
+// pop
+
+//
+
+
+
+
+
+
+classes = ["math",
+    "reading",
+    "gym",
+    10,
+    10.23,
+    true,
+    "reading"];
+
+
+
+var bookBag=[ "pencils","pens","notebook","fiction book","homework"]
+bookBag.push("sketch book")
+console.log(bookBag)
+
+bookBag.pop()
+console.log(bookBag)
+
+console.log(bookBag.length)
+
+function amazonCart(item){
+    itemInCart.push(item);
+    console.log(itemInCart)
+}
+
+amazonCart(["pen","pencil","notebook"])
+
+itemsInCart
