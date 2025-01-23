3afc4966d1afe832f9025f15213a2294a358ec93
NashaanO <Noglesby527@boyslatin.net>
2025-01-23 20:08:44 +0000

    While Loops notes completed

---
+++ unit 4/WhileLoops.js
@@ -0,0 +1,41 @@
+// Loops = A loop is a special
+// construct that repeats a set
+// of code instructions over and
+// over under specific 
+// conditions. 
+
+// While Loops = This is a special type of 
+// loop where so long as a condition is TRUE
+// it will repeat the code instructions over
+// and over. 
+
+var x = 2 // this is an ex of an assignment 
+// operator - giving a variable a value 
+
+// = : assignment operator
+// == : comparison operator (same as)
+// != : comparison operator (not the same as)
+
+while (8 / x == 4){ 
+    console.log('This message will repeat forever.')
+} 
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Loops Title</title>
+</head>
+
+
+10 == 10 && 2 < 4
+
+"3" == 3
+
+function timer() {
+    var intervl = 60
+    while (interval > 0)
+  console.log('countdown' + interval)  
+interval -=1
+}
+console.log("times up")
