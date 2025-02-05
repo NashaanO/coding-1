8c3629787bd0e67fad669ffc0c8f1fef986b1e10
NashaanO <Noglesby527@boyslatin.net>
2025-02-05 20:11:49 +0000

    ForLoops

---
+++ unit 4/forloops.js
@@ -0,0 +1,22 @@
+// For Loop = is a type of loop that iterated through
+//A list.
+//A list has a finite loop; it has a start point
+//And an end point; these are typically the first
+// and last item in the list
+
+groceryList = ['apple','mango','water',
+]
+
+let gradeBookList =[80, 70, 68, 93, 92]
+let x = 0
+for (x; x <gradeBookList.length; x+=1){
+    console.log(gradeBookList) [x] + 5
+    if (gradeBookList)[x] > 75 ; {
+        gradeBookList[x] + 5 ;
+
+    }
+    console.log(gradeBookList[x])
+    
+}
+
+//console.log(gradebookList[x] + 5)
